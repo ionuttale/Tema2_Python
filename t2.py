70163aa496eedb3523ffb9854b02f58d722e46f3
ramonacraciun <ramona.craciunn@gmail.com>
2023-12-01 18:04:00 +0200

    ceva

---
+++ t2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+dataframe=pd.read_csv('data.csv')
+#plt.plot(dataframe)
+#plt.show()
+
+#X=7
+dataframe=pd.read_csv('data.csv',nrows=7)
+#plt.plot(dataframe)
+#plt.show()
+
+#Y=14
+dataframe=pd.read_csv('data.csv', usecols=['Durata','Puls'], nrows=14)
+plt.plot(dataframe)
+plt.show()
